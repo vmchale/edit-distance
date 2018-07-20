2763b1c82299d629fd84d0859df8905898627231
Vanessa McHale <vanessa.mchale@iohk.io>
2018-07-20 02:28:59 -0500

    add haskell benchmarks for even 🌶️er comparison

---
+++ hs/bench/Bench.hs
@@ -0,0 +1,10 @@
+module Main (main) where
+
+import           Criterion.Main
+import           Text.EditDistance
+
+main :: IO ()
+main =
+    defaultMain [ bgroup "edit distance"
+                      [ bench "levenshteinDistance" $ nf (levenshteinDistance defaultEditCosts "exclude") "excude" ]
+                ]
